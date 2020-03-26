f0d48af2bac1afaf4b753f4ab184b16cf56a0d91
fangyuandinggithub <3026354242@qq.com>
2020-03-26 15:45:59 +0800

    Initial Commit

---
+++ SupervisionSystem/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SupervisionSystem
+//
+//  Created by fangyuan ding on 2020/3/26.
+//  Copyright © 2020 fangyuan ding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SupervisionSystemTests/SupervisionSystemTests.m
@@ -0,0 +1,37 @@
+//
+//  SupervisionSystemTests.m
+//  SupervisionSystemTests
+//
+//  Created by fangyuan ding on 2020/3/26.
+//  Copyright © 2020 fangyuan ding. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SupervisionSystemTests : XCTestCase
+
+@end
+
+@implementation SupervisionSystemTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
